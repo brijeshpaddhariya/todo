59c3cd4fb1f6c1c34ae79f616c60027e77cfbd7f
Brijesh <brijeshpaddhariya142@gmail.com>
2021-08-02 19:32:42 +0530

    create todo

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Todo::all();
+        return response()->json([
+            'todos' => $data
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+    
+        $data = Todo::create($request->all());
+
+        return response()->json([
+            'todo' => $data
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todo $todo)
+    {
+        return response()->json([
+            'todo' => $todo
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Todo $todo, $id)
+    {
+        $data = Todo::find($id);
+        return response()->json([
+            'todo' => $data
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+    
+        $data = $todo->update($request->all());
+
+        return response()->json([
+            'success' => $data
+        ]);
+    
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $data = Todo::find($id)->delete();
+
+        return response()->json([
+            'success' => $data
+        ]);
+    }
+}
